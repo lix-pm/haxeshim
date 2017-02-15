f528015c68e9b44e8d2bb8c9e50ea48284598c7e
back2dos <back2dos@gmail.com>
2017-02-15 14:31:19 +0100

    Add some tests.

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -3,8 +3,31 @@ package ;
 class RunTests {
 
   static function main() {
-    travix.Logger.println('it works');
-    travix.Logger.exit(0); // make sure we exit properly, which is necessary on some targets, e.g. flash & (phantom)js
+    var runner = new haxe.unit.TestRunner();
+
+    runner.add(new TestResolution());
+
+    travix.Logger.exit(
+      if (runner.run()) 0
+      else 500
+    );
   }
   
+}
+
+class TestResolution extends haxe.unit.TestCase {
+  function testNext() {
+    var r = new haxeshim.Resolver(Sys.getCwd(), null, Haxelib, function (v) throw 'assert');
+    assertEquals(
+      ["--macro", "Sys.println('before')", "--next", "-lib","tink_core","-main","Main","--interp"].join('\n')
+      ,r.resolve(['tests/build.hxml'], function (libs) {
+        var ret = [];
+        for (l in libs) {
+          ret.push('-lib');
+          ret.push(l);
+        }
+        return ret;
+      }).join('\n'));
+
+  }
 }
