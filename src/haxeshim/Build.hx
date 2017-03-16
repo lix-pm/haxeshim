4dce78d5989ad27128991a43a400c2d1caa6bbd4
back2dos <back2dos@gmail.com>
2017-03-16 14:04:03 +0100

    Prevent duplicate prefixing. Fixes #5.

--- src/haxeshim/Build.hx
+++ src/haxeshim/Build.hx
@@ -1,14 +1,18 @@
 package haxeshim;
 
 import haxe.macro.*;
+
 using sys.io.File;
+using StringTools;
 
 class Build { 
-
+  static var NODE_PREFIX = '#!/usr/bin/env node\n\n';
   static macro function postprocess() {
     Context.onAfterGenerate(function () {
       var file = Compiler.getOutput();
-      file.saveContent('#!/usr/bin/env node\n\n'+file.getContent());
+      var old = file.getContent();
+      if (!old.startsWith(NODE_PREFIX))
+        file.saveContent(NODE_PREFIX + old);
     });
     return null;
   }
