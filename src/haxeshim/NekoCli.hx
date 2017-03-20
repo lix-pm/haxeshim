d21308f712ab5c653372458204fae160ecf09d77
back2dos <back2dos@gmail.com>
2017-03-20 12:44:24 +0100

    Add neko command. Fixes #3.

---
+++ src/haxeshim/NekoCli.hx
@@ -0,0 +1,24 @@
+package haxeshim;
+
+class NekoCli {
+
+  static function main() {
+    var installation = Scope.seek().haxeInstallation;
+    var env = installation.env();
+    
+    if (Scope.IS_WINDOWS)
+      Sys.putEnv('PATH', env['PATH']);
+    var binary = 
+      if (Scope.IS_WINDOWS) 'neko.exe';
+      else 'neko';
+    Sys.exit(
+      switch Exec.sync(installation.nekoPath + '/$binary', Sys.getCwd(), Sys.args(), env) {
+        case Success(c):
+          c;
+        case Failure(e):
+          e.code;
+      }
+    );
+  }
+  
+}
