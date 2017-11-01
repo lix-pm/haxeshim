9ae2886a9fdfe09fdc3c0dc82b3bdab17a961242
Juraj Kirchheim <back2dos@googlemail.com>
2017-11-01 18:18:14 +0100

    Add resolution parsing.

--- src/haxeshim/LibResolution.hx
+++ src/haxeshim/LibResolution.hx
@@ -1,7 +1,17 @@
 package haxeshim;
 
-@:enum abstract LibResolution(String) {
+using tink.CoreApi;
+
+@:enum abstract LibResolution(String) to String {
   var Scoped = 'scoped';
   var Mixed = 'mixed';
   var Haxelib = 'haxelib';
+
+  static public function parse(s:String)
+    return switch s {
+      case 'scoped': Success(Scoped);
+      case 'mixed': Success(Mixed);
+      case 'haxelib': Success(Haxelib);
+      default: Failure(new Error(UnprocessableEntity, 'Invalid lib resolution strategy `$s`'));
+    }
 }
