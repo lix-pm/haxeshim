6f3ff1085eef433bd1818d782802f54e86791b4b
Juraj Kirchheim <juraj.kirchheim@doclerholding.com>
2020-01-15 00:04:23 +0100

    Less aggressive post install.

--- src/haxeshim/PostInstall.hx
+++ src/haxeshim/PostInstall.hx
@@ -10,8 +10,8 @@ using sys.FileSystem;
 using haxe.io.Path;
 using StringTools;
 
-class PostInstall { 
-  
+class PostInstall {
+
   static var placeholder = Bytes.ofString("abcdefghijklmnopqrstufvwxyzABCDEFGHIJKLMNOPQRSTUFVWXYZ0123456789abcdefghijklmnopqrstufvwxyzABCDEFGHIJKLMNOPQRSTUFVWXYZ0123456789abcdefghijklmnopqrstufvwxyzABCDEFGHIJKLMNOPQRSTUFVWXYZ0123456789abcdefghijklmnopqrstufvwxyzABCDEFGHIJKLMNOPQRSTUFVWXYZ0123456789");
   static var exifier = haxe.crypto.Base64.decode(haxeshim.exify.Embed.binary());
   static var offset = {
@@ -26,74 +26,81 @@ class PostInstall {
     ret;
   }
 
-  static function makeExe(call:String) {    
+  static function makeExe(call:String) {
     var call = Bytes.ofString(call),
         replacer = Bytes.alloc(placeholder.length);
 
     replacer.fill(0, replacer.length, 0);
     replacer.blit(0, call, 0, call.length);
-    
+
     var buf = Bytes.alloc(exifier.length);
-    
+
     buf.blit(0, exifier, 0, buf.length);
     buf.blit(offset, replacer, 0, replacer.length);
-    
+
     return buf;
   }
 
-  static function exify(dir, source, mayFail) {    
-    
+  static function exify(dir, source) {
+
     for (name in ['haxe', 'haxelib', 'neko']) {
       var exe = '$dir/$name.exe';
-      if (exe.exists() && !'$exe.bak'.exists())
-        exe.rename('$exe.bak');
       try {
         exe.saveBytes(makeExe('node "$source/${name}shim.js"'));
-      }
-      catch (e:Dynamic) 
-        if (!mayFail(name)) {
-          Sys.println('failed to shim $name');
-          Sys.exit(500);
+        var code = try Sys.command(exe) catch (e:Dynamic) 500;
+        if (code != 0) {
+          Sys.println('Warning: Windows will not allow executing shimmed $name.exe. It will be removed.');
+          try exe.deleteFile()
+          catch (e:Dynamic) {
+            Sys.println('ERROR: Removing $name.exe has also failed.');
+            Sys.println('At this point, things are probably badly broken.');
+            Sys.println('Please open $dir and see if you can clean it up.');
+            Sys.println('Good luck ...');
+            Sys.exit(500);
+          }
+          break;
         }
+      }
+      catch (e:Dynamic) {
+        Sys.println('failed to shim $name');
+        Sys.exit(500);
+      }
     }
 
   }
-  
+
   static var GLOBAL:Bool = !!(untyped process.env["npm_config_global"]); //wohooo \o/
 
   static var WINDOWS = Sys.systemName() == 'Windows';
 
   static function which(name) {
-    return 
-      Std.string(ChildProcess.spawnSync(if (WINDOWS) 'where' else 'which', [name]).stdout).split('\n').map(StringTools.trim);
+    return
+      Std.string(ChildProcess.spawnSync(if (WINDOWS) 'where' else 'which', [name]).stdout)
+        .split('\n').map(StringTools.trim).filter(f -> f != '');
   }
 
-  static function main() 
+  static function main()
     if (GLOBAL) {
 
       if (WINDOWS) {
-        var sources = js.Node.__dirname.replace('\\', '/'),
-            mayFail = 
-              switch Sys.command('haxe', ['--run', 'show-version']) {
-                case 0: function (c) return c == 'haxe';
-                default: function (_) return false; 
-              }
+        function isNpm(dir)
+          return '$dir/lix.cmd'.exists();
 
-        inline function process(dir)
-          exify(dir, sources, mayFail);
-
-        // trace('file: ' + js.Node.__dirname);
-        for (p in which('haxe')) 
+        var found = [];
+        for (p in which('haxe'))
           switch new Path(p) {
-            case { ext: 'cmd', dir: npm }:
-              
-              process(npm);
-              
-            case { ext: 'exe', dir: std } if (Lambda.foreach(['CHANGES.txt', 'CONTRIB.txt', 'LICENSE.txt'], function (file) return '$std/$file'.exists())):
-              
-              process(std);
+            case { ext: 'cmd', dir: npm } if (isNpm(npm)):
 
-            default:            
+              exify(npm, js.Node.__dirname.replace('\\', '/'));
+
+              if (found.length > 0 && '$npm/lix.cmd'.exists())
+                Sys.println(['Warning: the haxe executable bundled with lix is shadowed by:'].concat(found).join('\n  '));
+
+              break;
+
+            case { dir: other } if (!isNpm(other)):
+              found.push(p);
+            default:
           }
       }
 
